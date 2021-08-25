970a214a027636375bfc5be4891388733bce8427
Omar Labana <omarlabana@outlook.com>
2021-08-25 16:22:49 +0200

    Edit header comp

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <header className="flex items-center justify-between">
-            <div className="dancing-font">Lambo</div>
-            <ul className="flex gap-3">
-                <li>Home</li>
-                <li>Today's Dish</li>
+        <header className="flex items-center justify-between py-5">
+            <div className="dancing-font text-6xl">Lambo</div>
+            <ul className="flex gap-5 text-white text-2xl">
+                <li className="border-b-4 pb-1 px-1 border-yellow">Home</li>
+                <li className="text-gray-600">Today's Dish</li>
             </ul>
         </header>
     )
