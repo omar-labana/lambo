1caeb432c6930cc6a40c6659a5a4878570bdf01e
Omar Labana <omarlabana@outlook.com>
2021-08-25 15:29:49 +0200

    Add main components

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div>
+      <h1 className="dancing-font">Lambo</h1>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-
+import './custom.css'
 ReactDOM.render(
   <React.StrictMode>
     <App />
