52138ee113250d25a1babcd36963c0eed9297ff9
Omar Labana <omarlabana@outlook.com>
2021-08-26 19:08:31 +0200

    Add meals dispatch

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,18 @@ import Header from './Header'
 // import Footer from './Footer'
 // import Hero from './Hero'
 import Meal from './Meal'
+import { getMeals } from '../redux/slices/meals'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getMeals())
+  }, [dispatch])
+  const meals = useSelector(state => state.meals)
+  console.log(meals)
   return (
     <>
       <Header />
