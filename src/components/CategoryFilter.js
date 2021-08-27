ad3e9ff1cc9515333db02ab60ef4d5ef4b5ab23c
Omar Labana <omarlabana@outlook.com>
2021-08-27 13:17:51 +0200

    Add categoryfilter comp

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,21 @@
+
+const categories = ['All', 'Chicken', 'Pasta', 'Seafood'];
+
+const CategoryFilter = (props) => {
+  const { target, handleFilter } = props;
+
+  return (
+    <div className="flex items-center justify-center gap-5 py-10">
+      <h2 className="text-white text-3xl">Category Filter:</h2>
+      <select name="target" value={target} onChange={handleFilter} className="p-5 text-3xl">
+        {categories.map((category) => (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default CategoryFilter;
