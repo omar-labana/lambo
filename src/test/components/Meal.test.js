20312dd31c94acfe4475685a2e6aaf231fb7b428
Omar Labana <omarlabana@outlook.com>
2021-08-27 18:12:25 +0200

    Add testing for components and props

---
+++ src/test/components/Meal.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+import Meal from '../../components/Meal';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+const stl = {
+    strMeal: '1',
+    strMealThumb: "1",
+    type: "1",
+    idMeal: "1",
+}
+
+describe('Meal Component', () => {
+    const wrapper = shallow(<Meal meal={stl} key={stl.idMeal} />);
+
+    it('exists', () => {
+        expect(wrapper.find('Meal')).toBeTruthy();
+    });
+
+    it('has correct props', () => {
+        const propa = wrapper.props();
+        expect(propa).toBeTruthy();
+
+    });
+
+    it('has to match snap', () => {
+        const button = renderer.create(
+            <BrowserRouter><Meal meal={stl} key={stl.idMeal} /></BrowserRouter>
+
+        );
+        expect(button).toMatchSnapshot();
+    });
+});
