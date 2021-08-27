74b760dda7ed54b1634334f98001eb6b7fe02eef
Omar Labana <omarlabana@outlook.com>
2021-08-27 13:18:39 +0200

    Add two main vies comps

---
+++ src/views/Home.js
@@ -0,0 +1,50 @@
+import Hero from '../components/Hero'
+import Meal from '../components/Meal'
+
+import { getMeals } from '../redux/slices/meals'
+import { updateFilter } from '../redux/slices/filter'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import CategoryFilter from '../components/CategoryFilter';
+
+const Home = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getMeals())
+    }, [dispatch])
+    const meals = useSelector(state => state.meals.meals)
+    const target = useSelector((state) => state.filter.filter);
+    const handleFilter = (e) => {
+        const data = e.target.value;
+        dispatch(updateFilter(data));
+    };
+    const show = meals.filter((meal) => meal.type === target || target === 'All');
+
+    console.log(meals, show, target)
+    const renderMeals = (meals => meals.map(meal => <Meal meal={meal} key={meal.idMeal} />))
+    return (
+        <>
+            <Hero />
+            <CategoryFilter target={target} handleFilter={handleFilter} />
+            <div className="flex flex-col gap-10 mx-20">
+                {renderMeals(show)}
+            </div>
+        </>
+    )
+}
+
+export default Home
+
+
+
+// // import Hero from './Hero'
+// import Meal from './Meal'
+// import { getMeals } from '../redux/slices/meals'
+// import { useDispatch, useSelector } from 'react-redux'
+// import { useEffect } from 'react'
+// const dispatch = useDispatch()
+//   useEffect(() => {
+//     dispatch(getMeals())
+//   }, [dispatch])
+//   const meals = useSelector(state => state.meals)
+//   console.log(meals)
---
+++ src/views/Meal.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useLocation } from "react-router-dom";
+import { getDish } from '../redux/slices/dish'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+
+const Meal = () => {
+    const { state } = useLocation();
+    const { id } = state;
+
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getDish({ id: id }))
+    }, [dispatch])
+
+    const dish = useSelector(state => state.dish.dish)
+    const { strArea, strCategory, strInstructions, strMeal, strMealThumb, strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5 } = dish
+
+    console.log(dish)
+    return (
+        <div className="text-white text-2xl flex flex-col items center gap-10 mx-20">
+            <p className="dancing-font text-7xl">{strMeal}</p>
+            <div className="flex justify-around">
+                <div className="text-3xl flex flex-col gap-10">
+                    <p>Country: <span className="dancing-font">{strArea}</span></p>
+                    <p>Category: <span className="dancing-font">{strCategory}</span></p>
+                </div>
+                <img className="w-1/4" src={strMealThumb} alt={strMeal}></img>
+            </div>
+            <div className="">
+                <p><span className="dancing-font text-4xl">strIngredients: </span></p>
+                <ul>
+                    <li>{strIngredient1}</li>
+                    <li>{strIngredient2}</li>
+                    <li>{strIngredient3}</li>
+                    <li>{strIngredient4}</li>
+                    <li>{strIngredient5}</li>
+                </ul>
+            </div>
+            <p><span className="dancing-font text-4xl">How to preapare: </span>{strInstructions}</p>
+        </div>
+    )
+}
+
+export default Meal
