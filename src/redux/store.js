782e1d918ae3cc589d0cbc3779a22606656f4a9a
Omar Labana <omarlabana@outlook.com>
2021-08-27 13:18:51 +0200

    Add redux slices to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import mealsReducer from '../redux/slices/meals'
+import dishReducer from "../redux/slices/dish";
+import filterReducer from "../redux/slices/filter";
 export const store = configureStore({
-    reducer: { meals: mealsReducer },
+    reducer: {
+        meals: mealsReducer,
+        dish: dishReducer,
+        filter: filterReducer,
+    },
 })
