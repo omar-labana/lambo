99c854e568ae7d99bbbfdb3d4c51ee0cd406b551
Omar Labana <omarlabana@outlook.com>
2021-08-27 13:17:37 +0200

    Add routing to App comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,22 @@
 import Header from './Header'
-// import Footer from './Footer'
-// import Hero from './Hero'
-import Meal from './Meal'
-import { getMeals } from '../redux/slices/meals'
-import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from 'react'
+import Footer from './Footer'
+import Home from '../views/Home'
+import Meal from '../views/Meal'
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const App = () => {
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(getMeals())
-  }, [dispatch])
-  const meals = useSelector(state => state.meals)
-  console.log(meals)
+
   return (
     <>
       <Header />
-      {/* <Hero /> */}
-      {/* <Footer /> */}
-      <Meal />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/meal" component={Meal} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+      <Footer />
     </>
   )
 }
