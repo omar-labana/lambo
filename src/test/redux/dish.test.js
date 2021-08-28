842483923743a5b78541173b01e0f4bb507bb4bb
Omar Labana <omarlabana@outlook.com>
2021-08-28 15:46:52 +0200

    Add tests for reducers

---
+++ src/test/redux/dish.test.js
@@ -0,0 +1,9 @@
+import reducer from '../../redux/slices/dish'
+
+test('should return the initial state', () => {
+    expect(reducer(undefined, { })).toEqual(
+        {
+            dish: { },
+        }
+    )
+})
---
+++ src/test/redux/meals.test.js
@@ -0,0 +1,10 @@
+import reducer from '../../redux/slices/meals'
+
+test('should return the initial state', () => {
+    expect(reducer(undefined, { })).toEqual(
+        {
+            meals: [],
+            status: null,
+        }
+    )
+})
